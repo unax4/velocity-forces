553e2199db1f58c3614a4365bbf09dedd29d3e37
unax4 <131610168+unax4@users.noreply.github.com>
2023-05-06 11:30:04 +0200

    Add files via upload

--- funtzioak.f90
+++ funtzioak.f90
@@ -1,6 +1,6 @@
 module funtzioak 
 use tipoak
-public::fg,fmag,fharm,forb
+public::fg,fmag,fharm,forbi
 
 contains
 subroutine fg(vx,vy,fx,fy)
@@ -22,18 +22,19 @@ end subroutine fmag
 
 
 subroutine fharm(t,x,y,vx,vy,fx,fy)
-real(kind=dp),intent(in)::vx,vy
+real(kind=dp),intent(in)::vx,vy,x,y,t
 real(kind=dp),intent(out)::fx,fy
+real(kind=dp),parameter::pi=acos(-1.0_dp)
 fx=-(4*pi**2+1)*x-2*vx+sqrt(9*pi**4+10*pi**2+1)*cos(pi*t+atan(2*pi/(3*pi**2+1)))
 fy=-(4*pi**2+1)*y-2*vy+sqrt(9*pi**4+10*pi**2+1)*cos(pi*t+atan(2*pi/(3*pi**2+1)))
 end subroutine fharm
 
-subroutine forb(x,y,vx,vy,fx,fy)
-real(kind=dp),intent(in)::vx,vy
+subroutine forbi(x,y,vx,vy,fx,fy)
+real(kind=dp),intent(in)::x,y,vx,vy
 real(kind=dp),intent(out)::fx,fy
-real(kind=dp),parameter::pi=acos(-1.0_dp)
-fx=0.0
-fy=1.0_dp
-end subroutine forb
+real(kind=dp),parameter::pi=acos(-1.0_dp),mu=81.45_dp/82.45_dp,mup=1.0_dp/81.45_dp
+fx=x + 2*vy - mu*(x+mup)/((x+mup)**2+y**2)**(3/2) - mup*(x-mu)/((x-mup)**2+y**2)**(3/2)
+fy=y - 2*vx - mu*y/((x+mup)**2+y**2)**(3/2) - mup*y/((x-mup)**2+y**2)**(3/2)
+end subroutine forbi
 
 end module funtzioak
