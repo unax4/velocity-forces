4aa4804326aeb4ba9d7dec041e23379fdfb22f75
unax4 <131610168+unax4@users.noreply.github.com>
2023-05-06 14:37:13 +0200

    3.0

--- secondtaylor.f90
+++ secondtaylor.f90
@@ -1,39 +1,36 @@
 program taylor
 use tipoak
+use funtzioak
 real(kind=dp)::t,ta,tb,vint,xint,yint,fint,h
 real(kind=dp),dimension(2)::posi,velo,velint,for,forint
 integer::i,j,k
-integer,parameter::n=500
+integer::n
+n=1000
 
-
-!bektore gabe
-ta=0_dp
-tb=10_dp
+j=1
+!proiektila
+ta=0.0_dp
+tb=10.0_dp
 t=ta
 h=(tb-ta)/n
 
 
-posi=(/0.0_dp,4.0_dp/)
-velo=(/10.0_dp,10.0_dp/)
+posi=(/0.0_dp,0.0_dp/)
+velo=(/20.0_dp,20.0_dp/)
 
-
+open(unit=13,file="proiekt.dat",status="replace",action="write")
 
 do i=1,n
-write(unit=*,fmt="(3f12.4)")t,posi
+write(unit=13,fmt="(3f20.13)")t,posi
+write(unit=13,fmt=*)
+write(unit=13,fmt=*)
 
-!x=x+h*n*v+f*h**2/2
-!vint=v+f*h
-!fint=force(t+h,x,vint)
 
-!v=v+(f+fint)*h/2
-!t=t+h
-!f=force(t,x,v) !aire erresistentzia problementzat azken  jarri, f txikia bada indar kontserbatzaileekin konparatuz kendu eta f=fint)
-
-call force(t,posi(1),posi(2),velo(1),velo(2),for(1),for(2))
+call fg(velo(1),velo(2),for(1),for(2))
 posi=posi + h*velo + for*h**2/2
 
 velint=velo + for*h
-call force(t+h,posi(1),posi(2),velint(1),velint(2),forint(1),forint(2))
+call fg(velint(1),velint(2),forint(1),forint(2))
 
 velo=velo + (for+forint)*h/2
 t=t+h
@@ -45,14 +42,90 @@ end if
 
 end do
 
-contains
-subroutine force(temp,x,y,vx,vy,fx,fy)
-real(kind=dp),intent(in)::temp,x,y,vx,vy
-real(kind=dp),intent(out)::fx,fy
-real(kind=dp),parameter::c=-0.3_dp,m=2_dp,g=9.8_dp
 
-fx=-c*vx
-fy=-c*vy - g
-end subroutine force
+!magnetikoa
+t=0.0
+
+posi=(/1.0_dp,0.0_dp/)
+velo=(/0.0_dp,2*acos(-1.0_dp)/)
+h=0.01
+open(unit=14,file="mag.dat",status="replace",action="write")
+do i=1,1000
+write(unit=14,fmt="(3f20.13)")t,posi
+
+
+call fmag(velo(1),velo(2),for(1),for(2))
+posi=posi + h*velo + for*h**2/2
+
+velint=velo + for*h
+call fmag(velint(1),velint(2),forint(1),forint(2))
+
+velo=velo + (for+forint)*h/2
+t=t+h
+j=j+1
+if (j>15000) then
+
+exit
+end if
+
+end do
+
+!harmonikoa
+t=0.0
+h=(tb-t)/n
+posi=(/0.0_dp,0.0_dp/)
+velo=(/0.0_dp,0.0_dp/)
+
+open(unit=15,file="harm.dat",status="replace",action="write")
+do i=1,n
+write(unit=15,fmt="(3f20.13)")t,posi
+
+
+
+call fharm(t,posi(1),posi(2),velo(1),velo(2),for(1),for(2))
+posi=posi + h*velo + for*h**2/2
+
+velint=velo + for*h
+call fharm(t+h,posi(1),posi(2),velint(1),velint(2),forint(1),forint(2))
+
+velo=velo + (for+forint)*h/2
+t=t+h
+
+if (t==tb) then
+
+exit
+end if
+
+end do
+
+
+
+!grabitatorioa
+t=0.0
+tb=100
+n=10000
+h=(tb-t)/n
+posi=(/1.2_dp,0.0_dp/)
+velo=(/0.0_dp,1.049357509830_dp/)
+
+open(unit=16,file="orb.dat",status="replace",action="write")
+do i=1,n
+write(unit=16,fmt="(3f20.13)")t,posi
+write(unit=16,fmt=*)
+write(unit=16,fmt=*)
+
+
+call forbi(posi(1),posi(2),velo(1),velo(2),for(1),for(2))
+posi=posi + h*velo + for*h**2/2
+
+velint=velo + for*h
+call forbi(posi(1),posi(2),velint(1),velint(2),forint(1),forint(2))
+
+velo=velo + (for+forint)*h/2
+t=t+h
+
+
+end do
+
 
 end program taylor
